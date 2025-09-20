cc914fe05565c9872850029583a82351dadf0672
André Luiz <andreluiz.ddev@gmail.com>
2025-09-20 01:24:40 -0300

    adiciona endpoint simples de health

--- get_data.v
+++ get_data.v
@@ -3,6 +3,11 @@ module main
 import veb
 import shareds.web_ctx
 
+@['/ping'; get]
+pub fn (app &App) ping(mut ctx web_ctx.WebCtx) veb.Result {
+	return ctx.ok('pong')
+}
+
 @['/']
 pub fn (app &App) index(mut ctx web_ctx.WebCtx) veb.Result {
 	return ctx.ok('funcionou! on port ${app.port}')
