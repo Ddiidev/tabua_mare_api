ee78664e74dbf4f1c733ebccaddd12d3f548818c
André Luiz <andreluiz.ddev@gmail.com>
2026-07-14 16:39:42 -0300

    build: isolate local A B instances

--- dockerfiles/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Copia o banco SQLite para o volume compartilhado apenas se ainda não existir
-if [ ! -f /app/data/taubinha.sqlite ]; then
-    cp /app/taubinha.sqlite /app/data/taubinha.sqlite
-fi
-exec ./TabuaMareAPI "$PORT"
